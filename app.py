5ea52eda6e918fb299eb27e7d77de3076088cbd7
Meloore <demayh97@gmail.com>
2024-09-06 20:35:01 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
-from dash import Dash, html
+from dash import Dash, html, dcc
+import numpy as np
+import pandas as pd
+import plotly.express as px
+
 
 app = Dash(__name__)
 server = app.server
 
-app.layout = html.H1('Hellow World!')
+app.layout = html.H1('Hello World!')
 
 if __name__ == '__main__':
     app.run(debug=True)
